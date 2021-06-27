b2c3b19f0b44dda91a753bff96285502314ca248
vishweshbajpai <vishweshbajpai16@gmail.com>
2021-06-27 23:11:33 +0530

    updated comments section

--- js/post.js
+++ js/post.js
@@ -44,6 +44,7 @@ function commentClickHandler(comment) {
     div.appendChild(p);
     p.innerText = comment;
     document.getElementById("addNewComment").prepend(div);
+
     document.getElementById("textarea").value = "";
   }
 }
